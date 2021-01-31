dba3961bd5fa13cd36eafa27b47264a6ede12094
bemayer <bemayer@ucalgary.ca>
2021-01-30 17:10:42 -0700

    Added score functionality and score display

--- Scripts/TinderGameplay.gd
+++ Scripts/TinderGameplay.gd
@@ -16,25 +16,38 @@ const DATA := {
 }
 
 const COLORS := {
-	"red" : Color(1.0, 0.0, 1.0),
-	"green" : Color(0.0, 1.0, 0.0),
-	"blue" : Color(0.0, 0.0, 1.0)
+	"red" : Color.red,
+	"green" : Color.green,
+	"blue" : Color.blue,
+	"orange" : Color.orange,
+	"purple" : Color.purple
 }
+
+var score:int = 0;
+var currentColorKey
+onready var correctColorKey = COLORS.keys()[randi() % COLORS.keys().size()]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Score.text = str(score)
+	generate_new_monster()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func proccess_found_swipe() -> void:
+	if (currentColorKey == correctColorKey) :
+		score += 1
+		$Score.text = str(score)
+		print("score is ", score)
 	
+
 func generate_new_monster() -> void:
-	var colorKey = COLORS.keys()[randi() % COLORS.keys().size()]
-	var color = COLORS[colorKey]
+	currentColorKey = COLORS.keys()[randi() % COLORS.keys().size()]
+	var color = COLORS[currentColorKey]
 	monster.change_head_color(color)
 	monster.change_torso_color(color)
 	monster.change_legs_color(color)
@@ -44,4 +57,5 @@ func _on_LostButton_pressed():
 
 
 func _on_FoundButton_pressed():
+	proccess_found_swipe()
 	generate_new_monster()
